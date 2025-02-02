117d00515f60e13c7e030b07340361d357e18d4c
Nik Mohd Fairus Ismail <nmfairus@jtm.gov.my>
2025-02-02 17:22:53 +0800

    Add Dashboard component with welcome message

---
+++ src/components/users/Dashboard.vue
@@ -0,0 +1,8 @@
+<script></script>
+<template>
+    <div>
+      <h1 class="text-2xl  mb-4">Dashboard</h1>
+      <p>Selamat datang ke halaman dashboard!</p>
+    </div>
+  </template>
+<style></style>
